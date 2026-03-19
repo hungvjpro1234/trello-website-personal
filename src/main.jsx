58520262b6981e4e9e1f87679cf3e8c2b8f52f32
Manh Hung <manhung19112003@gmail.com>
2026-03-19 21:46:06 +0700

    Complete dark mode and light mode setting

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { CssBaseline } from '@mui/material'
-import { ThemeProvider } from '@mui/material/styles'
+// import { ThemeProvider } from '@mui/material/styles'
+// CssVarsProvider để sử dụng tính năng CSS Variables của MUI, giúp tối ưu hóa hiệu suất và hỗ trợ dynamic theming
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import theme from './theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    {/* Sử dụng CssVarsProvider để áp dụng theme cho toàn bộ ứng dụng */}
+    <CssVarsProvider theme={theme}>
       <CssBaseline />
       <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+    </CssVarsProvider>
+  </React.StrictMode>
 )
